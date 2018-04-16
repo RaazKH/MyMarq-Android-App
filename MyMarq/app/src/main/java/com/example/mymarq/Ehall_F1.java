f599192dc95e7a6e2af8cca5cb74f33db33c3373
Jeremy <horkupine0@gmail.com>
2018-04-15 20:12:16 -0500

    Implementing Jeremy's code version

--- MyMarq/app/src/main/java/com/example/mymarq/Ehall_F1.java
+++ MyMarq/app/src/main/java/com/example/mymarq/Ehall_F1.java
@@ -1,11 +1,22 @@
 package com.example.mymarq;
 
 import android.content.Intent;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class Ehall_F1 extends AppCompatActivity {
+
+    private TouchImageView image;
+    private TextView scrollPositionTextView;
+    private TextView zoomedRectTextView;
+    private TextView currentZoomTextView;
+    private DecimalFormat df;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,27 +27,61 @@ public class Ehall_F1 extends AppCompatActivity {
         Intent intent = getIntent();
         String floornum = intent.getStringExtra(EngineeringHall.EXTRA_MESSAGE);
 
-        if(floornum.equals("Floor 1"))
+
+        //
+        // DecimalFormat rounds to 2 decimal places.
+        //
+        df = new DecimalFormat("#.##");
+        scrollPositionTextView = (TextView) findViewById(R.id.scroll_position);
+        zoomedRectTextView = (TextView) findViewById(R.id.zoomed_rect);
+        currentZoomTextView = (TextView) findViewById(R.id.current_zoom);
+        switch (floornum){
+            case "Floor 1": image = findViewById(R.id.FL1); break;
+            case "Floor 2": image = findViewById(R.id.FL2); break;
+            case "Floor 3": image = findViewById(R.id.FL3); break;
+            case "Floor 4": image = findViewById(R.id.FL4); break;
+            case "Lower Level": image = findViewById(R.id.FLB); break;
+            default: throw new IllegalArgumentException("Invalid Floor");
+        }
+        /*if(floornum.equals("Floor 1"))
         {
-            findViewById(R.id.FL1).setVisibility(View.VISIBLE);
+            image = findViewById(R.id.FL1);
         }
         else if(floornum.equals("Floor 2"))
         {
-            findViewById(R.id.FL2).setVisibility(View.VISIBLE);
+            image = findViewById(R.id.FL2);
         }
         else if(floornum.equals("Floor 3"))
         {
-            findViewById(R.id.FL3).setVisibility(View.VISIBLE);
+            image = findViewById(R.id.FL3);
         }
         else if(floornum.equals("Floor 4"))
         {
-            findViewById(R.id.FL4).setVisibility(View.VISIBLE);
+            image = findViewById(R.id.FL4);
         }
         else if(floornum.equals("Lower Level"))
         {
-            findViewById(R.id.FLB).setVisibility(View.VISIBLE);
-        }
+            image = findViewById(R.id.FLB);
+        }*/
+        image.setVisibility(View.VISIBLE);
+        //
+        // Set the OnTouchImageViewListener which updates edit texts
+        // with zoom and scroll diagnostics.
+        //
+        image.setOnTouchImageViewListener(new TouchImageView.OnTouchImageViewListener() {
 
+            @Override
+            public void onMove() {
+                PointF point = image.getScrollPosition();
+                RectF rect = image.getZoomedRect();
+                float currentZoom = image.getCurrentZoom();
+                boolean isZoomed = image.isZoomed();
+                scrollPositionTextView.setText("x: " + df.format(point.x) + " y: " + df.format(point.y));
+                zoomedRectTextView.setText("left: " + df.format(rect.left) + " top: " + df.format(rect.top)
+                        + "\nright: " + df.format(rect.right) + " bottom: " + df.format(rect.bottom));
+                currentZoomTextView.setText("getCurrentZoom(): " + currentZoom + " isZoomed(): " + isZoomed);
+            }
+        });
 
 
         //Used for testing if the Strings were coming from the previous string
